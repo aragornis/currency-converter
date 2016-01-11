3ce799ab3046308a2cf1a2b7a5fc217e9002011d
Aragornis <ma.dousset@gmail.com>
2016-01-11 22:38:34 +0100

    Implement downloader to fetch exchange rate from xml

---
+++ rates/downloader.py
@@ -0,0 +1,13 @@
+from urllib.request import urlopen
+from xml.etree import ElementTree
+
+def getExchangeRates(url = None):
+    ns = {'def': 'http://www.ecb.int/vocabulary/2002-08-01/eurofxref'}
+
+    root = ElementTree.parse(url if url is not None else defaultDownloader()).getroot()
+    for entry in root.findall('def:Cube/def:Cube/def:Cube', ns):
+        yield entry.get('currency'), float(entry.get('rate'))
+
+def defaultDownloader(self):
+    return urlopen('http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml')
+
---
+++ tests/test_downloader.py
@@ -0,0 +1,10 @@
+import unittest
+import os
+import codecs
+import rates.downloader
+
+class DownloaderTests(unittest.TestCase):
+    def test_sample(self):
+        result = dict(rates.downloader.getExchangeRates(os.path.join(os.path.dirname(__file__), 'exchange_rate_sample.xml')))
+        self.assertEqual(result, {'USD': 1.0888, 'JPY': 128.33, 'BGN': 1.9558})
+

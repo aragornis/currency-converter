d1545fa03733e369e163cdbe9ae871483952198c
Aragornis <ma.dousset@gmail.com>
2016-01-12 22:45:31 +0100

    Validate currency symbol and rate

--- rates/downloader.py
+++ rates/downloader.py
@@ -1,12 +1,18 @@
 from urllib.request import urlopen
 from xml.etree import ElementTree
+import re
 
 def getExchangeRates(url = None):
     ns = {'def': 'http://www.ecb.int/vocabulary/2002-08-01/eurofxref'}
 
     root = ElementTree.parse(url if url is not None else defaultDownloader()).getroot()
     for entry in root.findall('def:Cube/def:Cube/def:Cube', ns):
-        yield entry.get('currency').upper(), float(entry.get('rate'))
+        currency = entry.get('currency').upper()
+        if re.match('^[A-Z]+$', currency):
+            try:
+                yield currency, float(entry.get('rate'))
+            except ValueError:
+                pass # silently ignore invalid currency rate that could occur
 
 def defaultDownloader():
     return urlopen('http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml')

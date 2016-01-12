5b77c684dd8343cfd13936c19ee4a197bc7144b5
Aragornis <ma.dousset@gmail.com>
2016-01-12 22:37:57 +0100

    Improve fetcher logging

--- fetcher.py
+++ fetcher.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     # Download and store rates
     rates = Rates(db_file, True)
     for (currency, rate) in downloader.getExchangeRates():
-        print('Set exchange rate %s for EUR -> %s' % (rate, currency))
+        print("Set exchange rate %s for 'EUR' -> '%s'" % (rate, currency))
         rates.setRate(currency, rate)
 
     print("%s exchange rates have been retrieved and saved to %s" % (rates.getRatesCount(), db_file))
